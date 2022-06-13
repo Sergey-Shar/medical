51546e76734ccc7a14f880591dd41483a0c5805d
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-06-13 22:38:24 +0300

    add: adaptive style form feedback and write-review

--- scripts/reviews-all.js
+++ scripts/reviews-all.js
@@ -3,3 +3,17 @@
       color: "#000",
     });
   } )()
+
+const btnWriteReview = document.querySelector('.reviews__button')
+const btnCloseWriteReview = document.querySelector('.write-review__btn-close')
+const formWriteReview = document.querySelector('.write-review')
+btnWriteReview.addEventListener('click', (e) => {
+  e.preventDefault()
+  formWriteReview.style.transform = 'translateX(0)'
+  document.body.style.overflow = 'hidden'
+})
+btnCloseWriteReview.addEventListener('click', (e) => {
+e.preventDefault()
+formWriteReview.style.transform = 'translateX(-3000px)'
+  document.body.style.overflow = 'visible'
+})

67f29edadeed454a8fe09a2dfa1f473e1de2abcc
Dmitry Rakovich <dmitryrakovich@mail.ru>
2022-06-18 18:19:50 +0300

    fix: click on location

--- scripts/index.js
+++ scripts/index.js
@@ -307,21 +307,105 @@ e.preventDefault()
 })
 
 // клик на локацию
-
-const btnLocation = document.querySelector('.location')
+{
+  (function () {
+const btnLocation = document.querySelectorAll('.location')
 const modalCity = document.querySelector('.city__content')
 const modalMask = document.querySelector('.modal-mask')
 const btnCloseModalCity = document.querySelector('.city__content')
 
-
-btnLocation.addEventListener('click', () => {
-  modalMask.style.display = 'flex'
-  modalCity.style.display = 'flex'
-  navModal.classList.toggle('open')
+btnLocation.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modalMask.style.display = 'flex'
+    modalCity.style.display = 'flex'
+      if (navModal.className === 'nav-modal open') {
+        navModal.classList.toggle('open')
+      }
+  })
 })
 
 btnCloseModalCity.addEventListener('click', () => {
   modalCity.style.display = 'none'
   modalMask.style.display = 'none'
-  navModal.classList.toggle('open')
+  if(window.innerWidth < 480) {
+    navModal.classList.toggle('open')
+  }
+  else if (navModal.className === 'nav-modal open') {
+    navModal.classList.toggle('open')
+  }
 })
+
+  })()
+}
+
+// модальные окна: логин, напомнить пароль
+
+{
+  (function () {
+    const btnUser = document.querySelector('.header__user-btn')
+
+    const modalMask = document.querySelector('.modal-mask')
+
+    const modalLogin = document.querySelector('.login-modal__content')
+    const modalRemindPassword = document.querySelector('.remind-password__content')
+    const modalRemindPasswordFinish = document.querySelector('.remind-password__content-finish')
+
+    const btnRemindPassword = document.querySelector('.login-modal__link-remind')
+    const btnBack = document.querySelector('.remind-password__link-back')
+    const btnSubmitRemindPassword = document.querySelector('.remind-password__btn-submit')
+
+    const btnCloseModalLogin = document.querySelector('.login-modal__btn-close')
+    const btnCloseModalRemindPassword = document.querySelector('.remind-password__btn-close')
+
+    const closeModalTimeout = (sec) => {
+        setTimeout(() => {
+            modalMask.style.display = 'none'
+            modalRemindPasswordFinish.style.display = 'none'
+        }, sec)
+    } 
+
+    btnUser.addEventListener('click', (e) => {
+        modalMask.style.display = 'flex'
+        modalLogin.style.display = 'grid'
+
+    })
+
+    btnCloseModalLogin.addEventListener('click', (e) => {
+        modalMask.style.display = 'none'
+        modalLogin.style.display = 'none'
+    })
+
+    btnRemindPassword.addEventListener('click', (e) => {
+        modalLogin.style.display = 'none'
+        modalRemindPassword.style.display = 'flex'
+
+    })
+
+    btnBack.addEventListener('click', (e) => {
+        modalRemindPassword.style.display = 'none'
+        modalLogin.style.display = 'grid'
+
+    })
+
+    btnCloseModalRemindPassword.addEventListener('click', () => {
+        modalMask.style.display = 'none'
+        modalRemindPassword.style.display = 'none'
+    })
+
+    modalRemindPasswordFinish.addEventListener('click', (e) => {
+        if (e.target.className === 'remind-password__btn-close' || 'main-btn remind-password__btn-submit') {
+            modalMask.style.display = 'none'
+            modalRemindPasswordFinish.style.display = 'none'
+            closeModalTimeout(0)
+        }
+    })
+
+    btnSubmitRemindPassword.addEventListener('click',  (e) => {
+        modalRemindPassword.style.display = 'none'
+        modalRemindPasswordFinish.style.display = 'flex'
+        closeModalTimeout(3000)
+        }
+    )
+
+})()
+}
